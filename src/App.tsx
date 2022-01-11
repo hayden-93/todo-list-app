1e80fc4ad1ba2568a3e8eb23c916eb9ed30ee0a1
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-11 18:43:30 +0000

    feat: App shell - Installed react-feather dependency - Added logo.svg to public directory - Updated index.html to set the body as id root - Created Layout component and used in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { Moon } from "react-feather";
+
 import "./index.css";
+
+const Layout = (props) => {
+  return (
+    <>
+      <header className="flex justify-between p-4 border-b border-neutral-6 bg-neutral-2">
+        <img src="/logo.svg" alt="to-do app logo" className="h-6 w-auto" />
+        <Moon className="text-sky-9" />
+      </header>
+      <main className="bg-neutral-1">{props.children}</main>
+    </>
+  );
+};
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>To-Do list</h1>
-        <h1 className="text-3xl font-bold underline text-sky-9 font-body">Hello world!</h1>
-      </div>
-    </>
+    <Layout>
+      <h1 className="text-3xl font-bold underline text-sky-9 font-body">To-Do list</h1>
+    </Layout>
   );
 }
 
