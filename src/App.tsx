01bf9f7ed4d7d13d546837f18bf729d0479cbb9a
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-11 13:11:59 +0000

    build: Tailwind Tokens - Added custom color tokens to tailwind.config file - Tested new color token on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
     <>
       <div>
         <h1>To-Do list</h1>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <h1 className='text-3xl font-bold underline text-sky-9'>
+          Hello world!
+        </h1>
       </div>
     </>
   );
