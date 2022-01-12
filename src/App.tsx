3990150bb4f388fe3311773bbd61dc92b76e372f
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-12 10:38:15 +0000

    refactor: Layout - Refactored classes on the Layout in App.tsx - Set overflow-hidden and full height to html and body tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import "./index.css";
 const Layout = (props) => {
   return (
     <>
-      <header className="flex justify-between p-4 border-b border-neutral-6 bg-neutral-2">
+      <header className="relative flex items-center justify-between p-4 bg-neutral-2">
         <img src="/logo.svg" alt="to-do app logo" className="h-6 w-auto tablet:h-8 desktop:h-10" />
-        <Moon className="text-sky-9 desktop:h-8 desktop:w-8" />
+        <Moon className="text-sky-9 tablet:h-8 tablet:w-8" />
+        <span className="absolute bottom-0 left-0 right-0 border-b border-neutral-6" />
       </header>
-      <main className="h-full bg-neutral-1">{props.children}</main>
+      <main className="overflow-auto h-full bg-neutral-1">{props.children}</main>
     </>
   );
 };
