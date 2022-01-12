acf027362c3a802a3543bf231fb5fb10583f0ec6
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-12 13:36:13 +0000

    refactor: PR Change - Applied the forwardRef function to the Button component - Rendered the Button component in App - Moved the id root inside the body tag

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,16 @@
-export const Button = (props) => {
+import { forwardRef, ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, forwardedRef) => {
   return (
-    <button className="inline-flex justify-center items-center bg-sky-3 text-sky-11 font-regular text-base-lg leading-base-lg py-4 px-8">
+    <button
+      ref={forwardedRef}
+      className="inline-flex justify-center items-center bg-sky-3 text-sky-11 font-regular text-base-lg leading-base-lg py-4 px-8"
+    >
       {props.children}
     </button>
   );
-};
+});
