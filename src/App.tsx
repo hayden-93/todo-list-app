b03a6d2fc66a5afb306781da45c270a498173341
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-12 10:05:09 +0000

    feat: Tailwind breakpoint tokens - Added breakpoint classes to header logo and icon - Set body height to fill viewport in index.html - Added breakpoint tokens for tablet and desktop

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,16 @@ const Layout = (props) => {
   return (
     <>
       <header className="flex justify-between p-4 border-b border-neutral-6 bg-neutral-2">
-        <img src="/logo.svg" alt="to-do app logo" className="h-6 w-auto" />
-        <Moon className="text-sky-9" />
+        <img src="/logo.svg" alt="to-do app logo" className="h-6 w-auto tablet:h-8 desktop:h-10" />
+        <Moon className="text-sky-9 desktop:h-8 desktop:w-8" />
       </header>
-      <main className="bg-neutral-1">{props.children}</main>
+      <main className="h-full bg-neutral-1">{props.children}</main>
     </>
   );
 };
 
 function App() {
-  return (
-    <Layout>
-      <h1 className="text-3xl font-bold underline text-sky-9 font-body">To-Do list</h1>
-    </Layout>
-  );
+  return <Layout></Layout>;
 }
 
 export default App;
